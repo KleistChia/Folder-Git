f8ffe3cc637a47f0380045f1e62117c5cbfda35d
UF-CHAIWAIBIN\UF-ChaiWaiBin <kleistchia@gmail.com>
2018-02-26 18:18:18 +0800

    Updated on 26/Feb 6:17pm

---
+++ Week-2/Mon/SpringFramework/src/main/java/hello/Application.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author UF-ChaiWaiBin
+ */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+              return "Hello World!";
+            }
+        };
+    }
+
+  public static void main(String[] args) {
+      ApplicationContext context = 
+          new AnnotationConfigApplicationContext(Application.class);
+      MessagePrinter printer = context.getBean(MessagePrinter.class);
+      printer.printMessage();
+  }
+    
+}
